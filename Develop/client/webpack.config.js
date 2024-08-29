25d2dbedef6988e41a610bbe62b3033c75de517c
AndrewBlanchard <blanchard0andrew@gmail.com>
2024-08-29 12:45:13 -0500

    Updated Node.js engine requirements and fixed vulnerabilities

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
+        template: path.resolve(__dirname, 'index.html'),
         title: 'J.A.T.E'
       }),
       new MiniCssExtractPlugin({
